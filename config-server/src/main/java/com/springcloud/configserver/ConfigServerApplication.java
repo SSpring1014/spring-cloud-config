04365e986cfe84f6574996e289f30837555d9aac
SSpring1014 <33773760+SSpring1014@users.noreply.github.com>
2018-08-29 19:44:53 +0800

    服务端
    这是一个服务端

---
+++ config-server/src/main/java/com/springcloud/configserver/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.springcloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 配置中心的配置
+ * @author mzf
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
